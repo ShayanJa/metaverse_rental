05f52a574ee904e48e97eb97597b1faf9286ac7b
shayanja <shay.talebi@gmail.com>
2022-02-15 10:27:15 -0800

    add dotenv

--- contracts/interfaces/IAuction.sol
+++ contracts/interfaces/IAuction.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.0;
 
 interface IAuction {
 	function bid(address token, uint256 tokenId, uint256 amount) external;
-  function updateBid(address token, uint256 tokenId, uint256 amount);
+  function updateBid(address token, uint256 tokenId, uint256 amount) external;
   function removeBid(address token) external;
 }
